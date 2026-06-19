cc157420455f76c962fab8b6d2f47f5d7e916506
chrispsk <chrispsk@gmail.com>
2026-06-19 18:17:52 +0100

    Refactor driver.py for clarity and consistency

--- driver.py
+++ driver.py
@@ -2,16 +2,15 @@ from github import GitHub
 
 
 if __name__ == "__main__":
-
     owner = "moby"
-    repo = ["buildkit", "tool"]
+    repos = ["buildkit", "tool"]
     resources = ["issues", "commits", "pulls"]
-    gh = GitHub(owner, repo, resources)
 
-    data = gh.read()
+    github_client = GitHub(owner, repos, resources)
+
+    data = github_client.read()
 
     while data is not None:
         print(data)
-        # here do something with data (save in db)
-        print("############## GOING TO NEXT PAGE ################")
-        data = gh.read() 
+        print("############### GOING TO NEXT PAGE ###############")
+        data = github_client.read()
